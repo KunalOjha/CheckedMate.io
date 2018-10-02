1c11b503ccc9faf9d708fbf034f34f24c8e42058
Kunal Ojha <kunalojha12@gmail.com>
2018-10-02 00:13:20 -0500

    initial scaffolding of checklist app

---
+++ src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { AppComponent } from './app.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginPageComponent
+  ],
+  imports: [
+    BrowserModule,
+    NgbModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/login-page/login-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login-page',
+  templateUrl: './login-page.component.html',
+  styleUrls: ['./login-page.component.css']
+})
+export class LoginPageComponent implements OnInit {
+  images = [1, 2, 3].map(() => `https://picsum.photos/900/500?random&t=${Math.random()}`);
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
