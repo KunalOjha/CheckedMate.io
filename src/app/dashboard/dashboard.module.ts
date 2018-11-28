146fb1aa63121078cd52be4f2f15710e635bb8ce
Kunal Ojha <kunalojha12@gmail.com>
2018-11-27 23:59:15 -0600

    initial form setup, adding in css tachyons support

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard.component';
+import { TodoFormComponent } from './containers/todo-form/todo-form.component';
+
 
 @NgModule({
   imports: [
     CommonModule,
   ],
-  declarations: [DashboardComponent]
+  declarations: [DashboardComponent, TodoFormComponent]
 })
 export class DashboardModule { }
