61d7a1c2bedfd5843782dc06150792faa3cfd37e
Kunal Ojha <kunalojha12@gmail.com>
2019-03-02 23:12:12 -0600

    removing extraneous state property

--- src/app/store/reducers/index.ts
+++ src/app/store/reducers/index.ts
@@ -3,7 +3,6 @@ import {
   MetaReducer
 } from '@ngrx/store';
 
-import * as fromTasksSummary from '../reducers/tasksSummary.reducer';
 import * as fromUser from '../reducers/user.reducer';
 import * as fromTasks from '../reducers/tasks.reducer'
 
@@ -11,13 +10,11 @@ import { environment } from '../../../environments/environment';
 
 export interface IAppState {
   user: fromUser.State,
-  tasksSummary:  fromTasksSummary.State,
   taskList : fromTasks.State
 }
 
 export const reducers: ActionReducerMap<IAppState> = {
   user: fromUser.userReducer,
-  tasksSummary: fromTasksSummary.tasksSummaryReducer,
   taskList: fromTasks.taskListReducer
 };
 
